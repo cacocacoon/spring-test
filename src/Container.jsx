ef795bc4cca2897abd963463440c443c78373a2f
Luke Liu 劉信俞 <cacocacoon@gmail.com>
2020-09-22 16:25:20 +0800

    simplified

--- src/Container.jsx
+++ src/Container.jsx
@@ -3,59 +3,8 @@ import { DropTarget } from "react-dnd";
 import { Transition } from "react-spring/renderprops";
 import DraggableCard from "./Card";
 import { ItemTypes } from "./ItemTypes";
+import items from './items';
 import "./style.css";
-
-const ITEMS = [
-  {
-    id: 1,
-    text: "Write a cool JS",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)"
-  },
-  {
-    id: 2,
-    text: "Make it generic",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)"
-  },
-  {
-    id: 3,
-    text: "Write README",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)"
-  },
-  {
-    id: 4,
-    text: "examples",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)"
-  },
-  {
-    id: 5,
-    text: "Spam in Twitter",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)"
-  },
-  {
-    id: 6,
-    text: "???",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)"
-  },
-  {
-    id: 7,
-    text: "PROFIT",
-    height: 80,
-    description: "#a8edea → #fed6e3",
-    css: "linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)"
-  }
-];
 
 const spec = {};
 
@@ -67,17 +16,20 @@ function collect(connect) {
 
 class DroppableList extends React.Component {
   state = {
-    cards: ITEMS,
+    cards: items,
   }
 
   moveCard = (id, atIndex) => {
     const { index } = this.findCard(id);
     this.setState(state => {
       const { cards } = state;
-      [cards[atIndex], cards[index]] = [cards[index], cards[atIndex]];
+      const moveCard = cards[index];
+      cards[index] = undefined;
+      const newCards = cards.filter(card => card !== undefined);
+      newCards.splice(atIndex, 0, moveCard);
 
-      return { cards: [...cards] };
-    })
+      return { cards: newCards };
+    });
   }
 
   findCard = id => {
---
+++ src/items.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        id: 1,
+        text: "Write a cool JS",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)"
+    },
+    {
+        id: 2,
+        text: "Make it generic",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)"
+    },
+    {
+        id: 3,
+        text: "Write README",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)"
+    },
+    {
+        id: 4,
+        text: "examples",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)"
+    },
+    {
+        id: 5,
+        text: "Spam in Twitter",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)"
+    },
+    {
+        id: 6,
+        text: "???",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)"
+    },
+    {
+        id: 7,
+        text: "PROFIT",
+        height: 80,
+        description: "#a8edea → #fed6e3",
+        css: "linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)"
+    }
+];
